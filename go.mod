f7bfc9ec499c6daa21214afc5f62b82970b8c6b6
Luca Sepe <sepelu@valueteam.com>
2019-03-22 10:35:06 +0100

    minimal refactoring

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/lucasepe/playstation
+
+go 1.12
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/antchfx/htmlquery v1.0.0 // indirect
+	github.com/antchfx/xmlquery v1.0.0 // indirect
+	github.com/antchfx/xpath v0.0.0-20190319080838-ce1d48779e67 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v0.0.0-20180810133444-97ee4a9ee6ea // indirect
+	golang.org/x/net v0.0.0-20190320064053-1272bf9dcd53 // indirect
+	google.golang.org/appengine v1.5.0 // indirect
+)
---
+++ store.go
@@ -0,0 +1,76 @@
+package playstation
+
+import (
+	"github.com/gocolly/colly"
+)
+
+var (
+	WeeklyDealsUrls = map[string]string{
+		"it": "it-IT/grid/STORE-MSF75508-DOTW1",
+		"en": "en-US/grid/STORE-MSF77008-WEEKLYDEALS",
+	}
+
+	SearchUrls = map[string]string{
+		"it": "it-it/search/",
+		"en": "en-US/search/",
+	}
+
+	AddonsUrls = map[string]string{
+		"en": "en-US/grid/STORE-MSF77008-NEWPS4ADDONSCATE",
+		"it": "it-it/grid/STORE-MSF75508-ADDONSSEEALL",
+	}
+
+	AllGamesUrls = map[string]string{
+		"en": "en-US/grid/STORE-MSF77008-PS4ALLGAMESCATEG",
+		"it": "it-it/grid/STORE-MSF75508-PS4CAT",
+	}
+)
+
+type Game struct {
+	Title   string
+	Cover   string
+	Details string
+	Price   string
+}
+
+func Visit(rootUrl string) chan Game {
+	ch := make(chan Game)
+
+	go func() {
+		// [\p{Sc}]
+
+		collector := colly.NewCollector(
+			colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0"),
+			colly.AllowedDomains("store.playstation.com"),
+		)
+
+		collector.OnHTML("div[class*='grid-cell--game']", func(e *colly.HTMLElement) {
+			cover := e.ChildAttr("div.grid-cell__thumbnail img[src^='https://store.playstation.com/store/api/chihiro']", "src")
+
+			title := e.ChildText("div.grid-cell__body > a")
+			details := e.ChildText("div.grid-cell__bottom > grid-cell__details-container grid-cell__left-detail--detail-2")
+			price := e.ChildText("div.grid-cell__bottom > div.grid-cell__footer h3.price-display__price")
+
+			if price != "" {
+				ch <- Game{Title: title, Cover: cover, Details: details, Price: price}
+			}
+		})
+
+		collector.OnHTML("div.grid-footer-controls a.paginator-control__next", func(e *colly.HTMLElement) {
+			link := e.Request.AbsoluteURL(e.Attr("href"))
+			e.Request.Visit(link)
+		})
+
+		collector.OnRequest(func(r *colly.Request) {
+			//log.Println("visiting", r.URL.String())
+		})
+
+		collector.Visit(rootUrl)
+
+		collector.Wait()
+
+		close(ch)
+	}()
+
+	return ch
+}
